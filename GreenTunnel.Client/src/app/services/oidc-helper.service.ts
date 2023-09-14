d723cab6f68fd6827128f9b2e79d0c9c12099516
Abdelhamid Derrar <abdelhamid.derrar.ext@sateba.com>
2023-09-14 13:15:02 +0100

    update oicd authentication

--- GreenTunnel.Client/src/app/services/oidc-helper.service.ts
+++ GreenTunnel.Client/src/app/services/oidc-helper.service.ts
@@ -1,4 +1,4 @@
- 
+
 
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
@@ -11,62 +11,64 @@ import { ConfigurationService } from './configuration.service';
 import { DBkeys } from './db-keys';
 import { LoginResponse } from '../models/login-response.model';
 
+import { environment } from '../../environments/environment';
+
 @Injectable()
 export class OidcHelperService {
 
-  private readonly clientId = 'greentunnel_spa';
-  private readonly scope = 'openid email phone profile offline_access roles greentunnel_api';
+    private readonly clientId = 'greentunnel_spa';
+    private readonly scope = 'openid email phone profile offline_access roles greentunnel_api';
 
-  private readonly tokenEndpoint = '/connect/token';
+    private readonly tokenEndpoint = environment.baseUrl + '/connect/token';
 
-  constructor(
-    private http: HttpClient,
-    private oauthService: OAuthService,
-    private configurations: ConfigurationService,
-    private localStorage: LocalStoreManager) {
+    constructor(
+        private http: HttpClient,
+        private oauthService: OAuthService,
+        private configurations: ConfigurationService,
+        private localStorage: LocalStoreManager) {
 
-  }
-
-
-  loginWithPassword(userName: string, password: string) {
-    const header = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
-    const params = new HttpParams()
-      .append('username', userName)
-      .append('password', password)
-      .append('client_id', this.clientId)
-      .append('grant_type', 'password')
-      .append('scope', this.scope);
-
-    return this.http.post<LoginResponse>(this.tokenEndpoint, params, { headers: header });
-  }
-
-  refreshLogin() {
-    const header = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
-    const params = new HttpParams()
-      .append('refresh_token', this.refreshToken)
-      .append('client_id', this.clientId)
-      .append('grant_type', 'refresh_token');
-
-    return this.http.post<LoginResponse>(this.tokenEndpoint, params, { headers: header });
-  }
-
-  get accessToken(): string {
-    return this.localStorage.getData(DBkeys.ACCESS_TOKEN);
-  }
-
-  get accessTokenExpiryDate(): Date {
-    return this.localStorage.getDataObject<Date>(DBkeys.TOKEN_EXPIRES_IN, true);
-  }
-
-  get refreshToken(): string {
-    return this.localStorage.getData(DBkeys.REFRESH_TOKEN);
-  }
-
-  get isSessionExpired(): boolean {
-    if (this.accessTokenExpiryDate == null) {
-      return true;
     }
 
-    return this.accessTokenExpiryDate.valueOf() <= new Date().valueOf();
-  }
+
+    loginWithPassword(userName: string, password: string) {
+        const header = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+        const params = new HttpParams()
+            .append('username', userName)
+            .append('password', password)
+            .append('client_id', this.clientId)
+            .append('grant_type', 'password')
+            .append('scope', this.scope);
+
+        return this.http.post<LoginResponse>(this.tokenEndpoint, params, { headers: header });
+    }
+
+    refreshLogin() {
+        const header = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+        const params = new HttpParams()
+            .append('refresh_token', this.refreshToken)
+            .append('client_id', this.clientId)
+            .append('grant_type', 'refresh_token');
+
+        return this.http.post<LoginResponse>(this.tokenEndpoint, params, { headers: header });
+    }
+
+    get accessToken(): string {
+        return this.localStorage.getData(DBkeys.ACCESS_TOKEN);
+    }
+
+    get accessTokenExpiryDate(): Date {
+        return this.localStorage.getDataObject<Date>(DBkeys.TOKEN_EXPIRES_IN, true);
+    }
+
+    get refreshToken(): string {
+        return this.localStorage.getData(DBkeys.REFRESH_TOKEN);
+    }
+
+    get isSessionExpired(): boolean {
+        if (this.accessTokenExpiryDate == null) {
+            return true;
+        }
+
+        return this.accessTokenExpiryDate.valueOf() <= new Date().valueOf();
+    }
 }
